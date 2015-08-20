0d4a37ee10cae8ec6ea6faee7afc440af7462bcf
Nikolas Stevenson-Molnar <nik.molnar@gmail.com>
2015-08-19 23:55:00 -0700

    Initial map with fire perimeters

--- nwfires_project/nwfires/settings_base.py
+++ nwfires_project/nwfires/settings_base.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'nwfires.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'nwfires_project/templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
